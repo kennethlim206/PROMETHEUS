4e8044c07d5dc68678300e5f5e15664a3b794533
Kenneth Lim <kennethlim@Kenneths-MacBook-Air.local>
2018-03-06 17:44:18 -0800

    Updated interface for submit/resubmit/report

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
 
 	os.popen("chmod +x ./processing_scripts/*.sh")
 
-	print "\n"
+	print ""
 	print "*********************************************************************************  "
 	print "*                        __       __         __                                 *  "
 	print "*   ________________    |  |  |  |  |  |    |    |   |    ________________|\\    * "
@@ -28,7 +28,7 @@ def main():
 	print "*                                                                               *  "
 	print "*********************************************************************************  "
 	print "         - THE NORDLAB RNA-SEQ INTERACTIVE PIPELINE EXPERIMENT YIELDER -           "
-	print "\n"
+	print "\n\n"
 
 	# Keeps user in the interface, until they choose to exit
 	task_exit = False
@@ -37,7 +37,7 @@ def main():
 
 		# Input task from user
 		print " ------------------------------------------------------------------------------- "
-		print "|            STEP 1: PLEASE CHOOSE THE ID OF YOUR TASK FILE OR EXIT             |"
+		print "|            STEP 1: PLEASE SELECT THE ID OF YOUR TASK FILE OR EXIT             |"
 		print " ------------------------------------------------------------------------------- "
 		print ""
 
@@ -52,7 +52,7 @@ def main():
 		print " ID   Task Sheet"
 		print " ------------------------------------------------------------------------------- "
 		for i in range(0, len(input_list)):
-			print " %i = %s" % (i, input_list[i])
+			print " %i = %s" % (i, input_list[i].replace(".txt", ""))
 
 		print ""
 		print " exit"
@@ -82,7 +82,7 @@ def main():
 		if not os.path.isfile(task_input_path):
 			sys.exit(" ERROR: Inputted task name does not exist.")
 		else:
-			print " You selected the input file: %s\n" % input_list[task_input]
+			print " You selected the input file: %s" % input_list[task_input].replace(".txt", "")
 
 
 
@@ -90,14 +90,15 @@ def main():
 		while function_input != "exit":
 
 			# Input function from user
+			print "\n\n"
 			print " ------------------------------------------------------------------------------- "
-			print "|                STEP 2: PLEASE CHOOSE FUNCTION(S) INPUT OR EXIT                |"
+			print "|                  STEP 2: PLEASE SELECT A FUNCTION(S) OR EXIT                  |"
 			print " ------------------------------------------------------------------------------- "
 			print " Note: Users can perform functions sequentially with the '->' symbol."
 			print " Example: get_slim -> align -> feature"
 			print ""
 
-			print " Input     Description"
+			print " Function     Description"
 			print " ------------------------------------------------------------------------------- "
 			# Display all functions from function table
 			function_files = open("./user_function_constructors/paths.txt", "r")
@@ -117,8 +118,6 @@ def main():
 			function_files.close()
 
 			print ""
-			print " report:[function] = reports progress (ex: report:feature)"
-			print " resubmit:[function] = resubmit single sample sbatch script"
 			print " exit"
 			print " ------------------------------------------------------------------------------- "
 			print ""
@@ -128,137 +127,165 @@ def main():
 			if function_input == "exit":
 				break
 
-			if "report:" in function_input:
-				function_report_name = function_input.split(":")[1]
+			# Parse and load function info
+			function_input = function_input.replace(" ",  "")
+			function_names_list = function_input.split("->")
+
+			function_path_list = []
+
+			# Add time together from all jobs
+			time_list = []
+
+			print " You have chosen the following function(s):"
+
+			for name in function_names_list:
 
 				# Error for non-existing user input
-				if function_report_name not in function_options:
-					sys.exit(" ERROR: Inputted function name does not exist: %s" % function_report_name)
-						
-				report.main(task_input_path, "./user_function_constructors/%s" % function_options[function_report_name])
+				if name not in function_options:
+					sys.exit(" ERROR: Inputted function name does not exist: %s" % name)
 
-			elif "resubmit:" in function_input:
-				function_report_name = function_input.split(":")[1]
+				function_input_path = "./user_function_constructors/%s" % function_options[name]
 
 				# Error for non-existing user input
-				if function_report_name not in function_options:
-					sys.exit(" ERROR: Inputted function name does not exist: %s" % function_report_name)
+				if not os.path.isfile(function_input_path):
+					sys.exit(" ERROR: Incorrect function path. Please check the input path in ./user_function_constructors/paths.txt")
 
-				resubmit.main(task_input_path, "./user_function_constructors/%s" % function_options[function_report_name])
+				function_path_list.append(function_input_path)
 
-			else:
+				print " %s" % name
 
-				# Parse and load function info
-				function_input = function_input.replace(" ",  "")
-				function_names_list = function_input.split("->")
+				# Get TIME function from function constructors
+				time_call = commands.getoutput("grep '<TIME>' %s" % function_input_path)
+				time_call = time_call.split("<TIME>")[1]
+				time_call = time_call.replace(" ", "")
+				time_call = time_call.replace("\n", "")
+				time_list.append(time_call)
 
-				function_path_list = []
+				# Get AUTO CALLED function from function constructors
+				auto_call = commands.getoutput("grep '<AUTO CALL>' %s" % function_input_path)
+				auto_call = auto_call.split("<AUTO CALL>")[1]
+				auto_call = auto_call.replace(" ", "")
+				auto_call = auto_call.replace("\n", "")
 
-				# Add time together from all jobs
-				time_list = []
+				if auto_call != "":
+					auto_path = "./user_function_constructors/%s" % auto_call
 
-				print " You have chosen the following function(s):"
+					# Error for non-existing autocall input
+					if not os.path.isfile(auto_path):
+						sys.exit(" ERROR: Incorrect <AUTO CALL> file name. Please check function constructor: %s" % path.rsplit("/", 1)[1])
 
-				for name in function_names_list:
-
-					# Error for non-existing user input
-					if name not in function_options:
-						sys.exit(" ERROR: Inputted function name does not exist: %s" % name)
-
-					function_input_path = "./user_function_constructors/%s" % function_options[name]
-
-					# Error for non-existing user input
-					if not os.path.isfile(function_input_path):
-						sys.exit(" ERROR: Incorrect function path. Please check the input path in ./user_function_constructors/paths.txt")
-
-					function_path_list.append(function_input_path)
-
-					print " %s" % name
-
-					# Get TIME function from function constructors
-					time_call = commands.getoutput("grep '<TIME>' %s" % function_input_path)
+					# Get AUTO CALLED function from function constructors
+					time_call = commands.getoutput("grep '<TIME>' %s" % auto_path)
 					time_call = time_call.split("<TIME>")[1]
 					time_call = time_call.replace(" ", "")
 					time_call = time_call.replace("\n", "")
 					time_list.append(time_call)
 
-					# Get AUTO CALLED function from function constructors
-					auto_call = commands.getoutput("grep '<AUTO CALL>' %s" % function_input_path)
-					auto_call = auto_call.split("<AUTO CALL>")[1]
-					auto_call = auto_call.replace(" ", "")
-					auto_call = auto_call.replace("\n", "")
+					function_path_list.append(auto_path)
 
-					if auto_call != "":
-						auto_path = "./user_function_constructors/%s" % auto_call
+					print " + AUTO CALLED: %s" % auto_call
 
-						# Error for non-existing autocall input
-						if not os.path.isfile(auto_path):
-							sys.exit(" ERROR: Incorrect <AUTO CALL> file name. Please check function constructor: %s" % path.rsplit("/", 1)[1])
+			
 
-						# Get AUTO CALLED function from function constructors
-						time_call = commands.getoutput("grep '<TIME>' %s" % auto_path)
-						time_call = time_call.split("<TIME>")[1]
-						time_call = time_call.replace(" ", "")
-						time_call = time_call.replace("\n", "")
-						time_list.append(time_call)
+			# Input function from user
+			print "\n\n"
+			print " ------------------------------------------------------------------------------- "
+			print "|                    STEP 3: PLEASE CHOOSE AN ACTION OR EXIT                    |"
+			print " ------------------------------------------------------------------------------- "
+			print " Note: If you selected multiple functions using the '->' symbol, the only action"
+			print " you may perform is 'submit'."
+			print ""
 
-						function_path_list.append(auto_path)
+			print " Action     Description"
+			print " ------------------------------------------------------------------------------- "
+			print " submit = submit your chosen function/string of functions to the sbatch queue"
 
-						print " + AUTO CALLED: %s" % auto_call
+			if "->" not in function_input:
+				print " report = check the progress of your chosen function"
+				print " resubmit = resubmit a singular sbatch script from your chosen function"
+			
+			print ""
+			print " exit"
+			print " ------------------------------------------------------------------------------- "
+			print ""
+
+			action_input = raw_input(" >>> ")
+
+			if action_input != "exit":
+
+				# Go into report progress
+				if action_input == "report" and "->" not in function_input:
+					report.main(task_input_path, "./user_function_constructors/%s" % function_options[function_input])
+
+				elif action_input == "resubmit" and "->" not in function_input:
+					resubmit.main(task_input_path, "./user_function_constructors/%s" % function_options[function_input])
 				
-				# Generate string input to give to queen
-				function_string = ""
-				for path in function_path_list:
-					function_string += "%s+" % path
+				elif action_input == "submit":
+					
+					# Generate string input to give to queen
+					function_string = ""
+					for path in function_path_list:
+						function_string += "%s+" % path
 
-				print ""
-				print " ------------------------------------------------------------------------------- "
-				print " Constructing your module ..."
-				print ""
+					print ""
+					print " ------------------------------------------------------------------------------- "
+					print " Constructing your module ..."
+					print ""
 
-				# Add time together
-				total_secs = 0
-				for tm in time_list:
-					time_parts = [int(s) for s in tm.split(':')]
-					total_secs += (time_parts[0] * 60 + time_parts[1]) * 60 + time_parts[2]
-				total_secs, sec = divmod(total_secs, 60)
-				hr, m = divmod(total_secs, 60)
-				time_display = "%d:%02d:%02d" % (hr, m, sec)
-				t = "%d:%02d:%02d" % (hr+queue_time, m, sec)
+					# Add time together
+					total_secs = 0
+					for tm in time_list:
+						time_parts = [int(s) for s in tm.split(':')]
+						total_secs += (time_parts[0] * 60 + time_parts[1]) * 60 + time_parts[2]
+					total_secs, sec = divmod(total_secs, 60)
+					hr, m = divmod(total_secs, 60)
+					time_display = "%d:%02d:%02d" % (hr, m, sec)
+					t = "%d:%02d:%02d" % (hr+queue_time, m, sec)
 
-				# Sumbit QUEEN module
-				cmd = "sbatch --time=%s ./processing_scripts/queen_submitter.sh %s %s" % (t, task_input_path, function_string[:-1])
-				
-				status = 0
-				ID = "Submitted job as 1738"
+					# Sumbit QUEEN module
+					cmd = "sbatch --time=%s ./processing_scripts/queen_submitter.sh %s %s" % (t, task_input_path, function_string[:-1])
+					
+					status = 0
+					ID = "Submitted job as 1738"
 
-				if not test:
-					status, ID = commands.getstatusoutput(cmd)
+					if not test:
+						status, ID = commands.getstatusoutput(cmd)
 
-				if status == 0:
-					ID_split = ID.split(" ")
-					ID = int(ID_split[3])
-					print " Your queen module has been submitted: %i" % ID
-					print " Estimated total run time: %s (+ time in queue)" % time_display
+					if status == 0:
+						ID_split = ID.split(" ")
+						ID = int(ID_split[3])
+						print " Your queen module has been submitted: %i" % ID
+						print " Estimated total run time: %s (+ time in queue)" % time_display
+					else:
+						sys.exit(" ERROR:\n%s" % ID)
+
+					print ""
+
+					sys.stdout.write(" Returning to step 2 ")
+					sys.stdout.flush()
+					time.sleep(1)
+
+					for i in range(0,3):
+						sys.stdout.write(".")
+						sys.stdout.flush()
+						time.sleep(1)
+
+					print ""
+					print ""
+
 				else:
-					sys.exit(" ERROR:\n%s" % ID)
-
-			print ""
-			print " Returning to step 2 ..."
-			time.sleep(3)
-
-			print ""
+					sys.exit(" ERROR: Chosen action does not exist.")
 
 
 
 		print " ------------------------------------------------------------------------------- "
-		print " Would you like to select a different dataset to work with? (Y/N)\n"
+		print " Would you like to select a different dataset to work with? (y/n)\n"
 		more_task = raw_input(" >>> ")
 
-		if more_task == "N":
-			task_exit = True
-		elif more_task == "Y":
+		if more_task == "y":
 			task_exit = False
+		elif more_task == "n":
+			task_exit = True
 		else:
 			sys.exit(" Incorrect input. Exiting program...")
 
